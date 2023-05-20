242267441df1e189b8fdda9db9ee8ea6ad3c51b4
Diana Dvoryak <dianadvoryak.w@gmail.com>
2023-05-20 21:58:21 +0200

    create: routing, controllers, views

---
+++ controllers/SiteController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\Application;
+use app\core\Controller;
+use app\core\Request;
+
+class SiteController extends Controller
+{
+    public function home()
+    {
+        $params = [
+            'name' => 'DianaDvoryak'
+        ];
+        return $this->render('home', $params);
+    }
+
+    public function contact()
+    {
+        return $this->render('contact');
+    }
+
+    public function handleContact(Request $request)
+    {
+        $body = $request->getBody();
+        return 'Handling submitting data';
+    }
+}
---
+++ core/Application.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\core;
+
+use app\core\Router;
+
+class Application
+{
+    public static string $ROOT_DIR;
+    public Router $router;
+    public Request $request;
+    public Response $response;
+    public static Application $app;
+    public Controller $controller;
+
+    public function __construct($rooPath)
+    {
+        self::$ROOT_DIR = $rooPath;
+        self::$app = $this;
+        $this->request = new Request();
+        $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
+    }
+
+    public function run()
+    {
+        echo $this->router->resolve();
+    }
+
+    public function getController(): Controller
+    {
+        return $this->controller;
+    }
+
+    public function setController(Controller $controller): void
+    {
+        $this->controller = $controller;
+    }
+}
